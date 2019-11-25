3cd06505a18a22259c9f56568a6c850662bc925e
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2019-11-25 18:42:17 +0100

    Personen Test erweitert

--- src/test/java/de/rat/PersonTest.java
+++ src/test/java/de/rat/PersonTest.java
@@ -1,5 +1,6 @@
 package de.rat;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,10 +12,16 @@ class PersonTest {
         Address address = new Address("Straße", 5, 12354, "Erfurt", "Germany");
         Account account = new Account();
         Department department = new Department("anvfsapfovb");
-
         Employee employee1 = new Employee("Steinbrecher", "Danny", "2019.11.15", address, account, department, null);
 
-        //Assertions.assertEquals(1, Werkzeug1.getToolId());
+        Assertions.assertEquals(address, employee1.getAddress());
+        Assertions.assertEquals(account, employee1.getAccount());
+        Assertions.assertEquals(department, employee1.getDepartment());
+        Assertions.assertEquals("Steinbrecher", employee1.getLastname());
+        Assertions.assertEquals("Danny", employee1.getFirstname());
+        Assertions.assertEquals("2019.11.15", employee1.getBirthday());
+        Assertions.assertEquals(null, employee1.getSupervisor());
+
 
     }
 
