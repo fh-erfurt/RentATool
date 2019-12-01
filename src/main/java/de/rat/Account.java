68afb4fb6c0750287dd67bf58a81b437a5d05ac8
bilal.alnaani@fh-erfurt.de <bilal0710>
2019-12-01 17:30:19 +0100

    die Klassen Address und Department sind fertig, schaut ihr sie bitte mal irgendwann

--- src/main/java/de/rat/Account.java
+++ src/main/java/de/rat/Account.java
@@ -1,6 +1,59 @@
 package de.rat;
 
+import java.time.LocalDate;
+
 public class Account {
+    private String role;
+    private String email;
+    private String password;
+    private LocalDate changed;
 
+    public Account(String role, String email, String password) {
+        this.role = role;
+        this.email = email;
+        this.password = password;
+        this.changed = LocalDate.now();
+    }
 
+    public String getRole() {
+        return role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDate getChanged() {
+        return changed;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setChanged() {
+        this.changed = LocalDate.now();
+    }
+    public void changeAccount(String role, String email, String password){
+        this.setRole(role);
+        this.setEmail(email);
+        this.setPassword(password);
+        this.setChanged();
+
+    }
+    public boolean createPasswort(){
+        return true;
+    }
 }
