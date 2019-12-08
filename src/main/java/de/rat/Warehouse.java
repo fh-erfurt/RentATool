79abb1693afa2b83b09544352fcdbb402308fa6e
Monschey <marco.petzold@fh-erfurt.de>
2019-12-08 21:35:36 +0100

    mmh geht nicht

--- src/main/java/de/rat/Warehouse.java
+++ src/main/java/de/rat/Warehouse.java
@@ -2,25 +2,64 @@ package de.rat;
 
 import java.util.ArrayList;
 
-public class Warehouse extends Station {
+public class Warehouse {
 
-    public Warehouse(String description, int numberOfBoxes, Address address){
-        super(description,numberOfBoxes,address);
+    private String CompanyName;
+    private ArrayList<Bill>openbills = new ArrayList<Bill>();
+    private ArrayList<Bill>closedbills = new ArrayList<Bill>();
+    private ArrayList<Tool> Stock= new ArrayList<Tool>();
+    private ArrayList<Employee> employees= new ArrayList<Employee>();
+
+
+    public Warehouse(String CompanyName) {
+        this.CompanyName=CompanyName;
     }
-    //private int numberofStocks;
 
-   // private ArrayList<> stock = new ArrayList<>;
-/*
-    public Warehouse(String description, int numberOfBoxes, Address address, int numberofStocks) {
-        super(description, numberOfBoxes, address);
-        this.numberofStocks=numberofStocks;
+    public String getCompanyName() {
+        return CompanyName;
+    }
 
-        for (int i = 0; i < numberofStocks; i++) {
+    public void setCompanyName(String companyName) {
+        CompanyName = companyName;
+    }
 
+    public ArrayList<Bill> getOpenbills() {
+        return openbills;
+    }
 
-        }
-    }*/
+    public void setOpenbills(ArrayList<Bill> openbills) {
+        this.openbills = openbills;
+    }
 
+    public ArrayList<Bill> getClosedbills() {
+        return closedbills;
+    }
 
+    public void setClosedbills(ArrayList<Bill> closedbills) {
+        this.closedbills = closedbills;
+    }
+
+    public ArrayList<Tool> getStock() {
+        return Stock;
+    }
+
+    public void setStock(ArrayList<Tool> stock) {
+        Stock = stock;
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public boolean closeBill(Customer customer){
+
+        int i= 3*4;
+
+        return true;
+    }
 }
 
