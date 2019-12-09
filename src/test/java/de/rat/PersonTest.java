d85e866f77293ed9336d381b7597bb2a7d179ded
danny <danny.steinbrecher@fh-erfurt.de>
2019-12-09 10:48:37 +0100

    PersonTest mit @Before angepasst

--- src/test/java/de/rat/PersonTest.java
+++ src/test/java/de/rat/PersonTest.java
@@ -1,31 +1,52 @@
 package de.rat;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.GregorianCalendar;
 
 
 class PersonTest {
 
-    // @BeforeEach
-    // Setup Departments
-    private Department deptRental = new Department("Verleih");
-    private Department deptLogistics = new Department("Logistik");
-    private Department deptManagement = new Department("Geschaeftsleitung");
+    //Variable declaration
+    private Department deptRental;
+    private Department deptLogistics;
+    private Department deptManagement;
 
-    // Setup Employees
-    private Employee empDanny = new Employee("Steinbrecher", "Danny", new GregorianCalendar(2019,GregorianCalendar.DECEMBER,15),
-        "Johannesstraße", 5, 99084, "Erfurt", "Germany", deptManagement, null);
-    private Employee empMichael = new Employee("Müller", "Michael", new GregorianCalendar(2017, GregorianCalendar.FEBRUARY,5),
-        "Michaelistraße", 17, 99086, "Erfurt", "Germany", deptLogistics, empDanny);
-    private Employee empJonas = new Employee("Casio", "Jonas", new GregorianCalendar(2000, GregorianCalendar.OCTOBER,30),
-        "Leipzigerstraße", 99, 99084, "Weimar", "Germany", deptRental, empDanny);
+    private Employee empDanny;
+    private Employee empMichael;
+    private Employee empJonas;
 
-    // Setup Customer
-    private Customer custMaria = new Customer("Schmidt", "Maria", new GregorianCalendar(2005, GregorianCalendar.AUGUST,29), "maria.schmidt@web.de",
-        "Weimarerlandstraße", 53, 99986, "Dresden", "Germany", "561616310651");
-    private Customer custLudwig = new Customer("Ebert", "Ludwig", new GregorianCalendar(1937, GregorianCalendar.DECEMBER,17), "crazyemail@web.de",
-        "Bahnhofsstraße", 16, 99067, "Gotha", "Germany", "01236/465854");
+    private Customer custMaria;
+    private Customer custLudwig;
+
+    private Company rentATool;
+
+    @BeforeEach
+    void setupDepartments(){
+       deptRental = new Department("Verleih");
+       deptLogistics = new Department("Logistik");
+       deptManagement = new Department("Geschaeftsleitung");
+    }
+
+    @BeforeEach
+    void setupEmployees() {
+        empDanny = new Employee("Steinbrecher", "Danny", new GregorianCalendar(2019, GregorianCalendar.DECEMBER, 15),
+                "Johannesstraße", 5, 99084, "Erfurt", "Germany", deptManagement, null);
+        empMichael = new Employee("Müller", "Michael", new GregorianCalendar(2017, GregorianCalendar.FEBRUARY, 5),
+                "Michaelistraße", 17, 99086, "Erfurt", "Germany", deptLogistics, empDanny);
+        empJonas = new Employee("Casio", "Jonas", new GregorianCalendar(2000, GregorianCalendar.OCTOBER, 30),
+                "Leipzigerstraße", 99, 99084, "Weimar", "Germany", deptRental, empDanny);
+    }
+
+    @BeforeEach
+    void setupCustomer() {
+        custMaria = new Customer("Schmidt", "Maria", new GregorianCalendar(2005, GregorianCalendar.AUGUST, 29), "maria.schmidt@web.de",
+                "Weimarerlandstraße", 53, 99986, "Dresden", "Germany", "561616310651",rentATool);
+        custLudwig = new Customer("Ebert", "Ludwig", new GregorianCalendar(1937, GregorianCalendar.DECEMBER, 17), "crazyemail@web.de",
+                "Bahnhofsstraße", 16, 99067, "Gotha", "Germany", "01236/465854", rentATool);
+    }
 
 
     @Test
