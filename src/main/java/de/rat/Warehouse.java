907233e697594e7a1b7b4c53431974e84b55b923
bilal.alnaani@fh-erfurt.de <bilal0710>
2019-12-26 14:42:42 +0100

    Warenhouse-Methode sind erledigt

--- src/main/java/de/rat/Warehouse.java
+++ src/main/java/de/rat/Warehouse.java
@@ -14,24 +14,42 @@ public class Warehouse {
 
     public boolean putToolInWarehouse(Tool tool){
         //TODO: Werkzeug einlagern
-        return true;
+        if(tool !=null)
+        {
+            this.Stock.add(tool);
+            return true;
+        }
+        return false;
     }
 
     public Tool removeToolFromWarehouse(Tool tool){
         //TODO: Werkzeug aus Lager nehmen und übergeben
         // TODO: Wird ein Werkzeug vom Lager in die Station gebracht, darf es nicht mehr im Lager liegen -> TEST (gehört zur Company)
-        return tool;
+        for (Tool foundedTool : Stock)
+        {
+            if(foundedTool.equals(tool))
+            {
+                if(foundedTool.getToolStatus() == ToolStatus.AVAILABLE) {
+                    Stock.remove(tool);
+                    return tool;
+                }
+            }
+        }
+        System.out.println("Werkzeug nicht im Lager vorhanden");
+        return null;
+
     }
 
     // TODO: Method aus der Company hier eingefügt - muss angepasst werden
     public Tool findToolInStockOfWarehouse( Tool tool){
-        for (Tool foundedTool : this.getStock()) {
+        for (Tool foundedTool : Stock) {
             if (foundedTool.equals(tool)) {
                 if(foundedTool.getToolStatus() == ToolStatus.AVAILABLE) {
                     return foundedTool;
                 }
             }
         }
+        System.out.println("Werkzeug nicht im Lager vorhanden");
         return null;
     }
 
