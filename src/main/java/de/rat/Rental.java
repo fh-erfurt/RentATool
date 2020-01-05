2022c8b095b7993e5d7c777c77b6bb2c8c78147c
Christian <christian.koenig@fh-erfurt.de>
2020-01-05 13:01:18 +0100

    Javadoc rental

--- src/main/java/de/rat/Rental.java
+++ src/main/java/de/rat/Rental.java
@@ -2,23 +2,46 @@ package de.rat;
 
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
-
+/**Represents an rental.
+ * @author ???
+ * @version ??
+ * @since ??
+ */
 public class Rental {
-
+    /** Creates an rental .
+     * @param openBills a list from open bills
+     * @param closedBills a list from closed bills
+     */
     private ArrayList<Bill> openBills = new ArrayList<Bill>();
     private ArrayList<Bill> closedBills = new ArrayList<Bill>();
 
+
+
+    /** Creates an rental .
+     */
     public Rental() {
     }
 
+    /** Creates an rental from the tool .
+     *  @param wantedTool the tool that would by rented
+     *  @param pickupStation the station which the tool was pickup
+     *  @param customer the customer that rented the tool
+     *  @param warehouse the the warehouse which the tool was removed in
+     *  store in the pickupStation
+     */
     public boolean rentATool(Tool wantedTool, Station pickupStation, Customer customer, Warehouse warehouse) {
 
-        // check if the station is full
+
+        /**check if the station is full
+         * @return false ????
+         */
         if(!pickupStation.checkStationLevel()){
             return false;
         }
 
-        // check if the searched Tool is in the warehouse
+        /**check if the searched Tool is in the warehouse
+         * @return false ????
+         */
         if(warehouse.removeToolFromWarehouse(wantedTool) == null){
             return false;
         }
@@ -35,28 +58,45 @@ public class Rental {
         return true;
     }
 
+
     public boolean returnTool(Tool wantedTool, Station removeStation, Customer customer, Warehouse warehouse, GregorianCalendar date){
+        /**Gets the wanted tool.
+         * @return false when the returnTool that store in the remove station
+         * is not the wanted tool, otherwise the wanted tool is store in the warehouse
+         */
         if(removeStation.removeToolFromBox(wantedTool) == null){
             return false;
         }
         warehouse.putToolInWarehouse(wantedTool);
 
+        /**Gets the open bill.
+         * @return false if there are no open bills from the customer
+         */
         Bill bill = findOpenBillFromCustomer(customer);
         if(bill == null){
             return false;
         }
-
+        /**Gets the rent process in which the wanted Tool was rented.
+         * @return false if there are no rent process
+         */
         RentProcess rentprocess = bill.findRentProcess(wantedTool);
         if(rentprocess == null){
             return false;
         }
 
+        /**Gets the rent process with the remove station an date.
+         * @return true if the bill was close
+         */
         rentprocess.completeRentProcess(removeStation, date);
         bill.closeBill(customer, 2);    //TODO: Discount?
 
         return true;
     }
 
+    /** Find a open bill from the customer.
+     * @return A class bill when the customer has a open bill, otherwise
+     * @return null if there are no open bills
+     */
     public Bill findOpenBillFromCustomer(Customer customer){
         for (Bill foundedBill : this.openBills) {
             if (foundedBill.getCustomer().equals(customer)) {
@@ -66,6 +106,9 @@ public class Rental {
         return null;
     }
 
+    /** Create a bill from the customer
+     * @return A class bill with the customer and the pickup station
+     */
     public Bill CreateOpenBillFromCustomer(Station pickupStation, Customer customer){
         Bill newBill = new Bill(customer, pickupStation);
         this.openBills.add(newBill);
