cd079e4d1744ef4054e3b12715ff0b6b0e5131fe
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2020-01-26 11:03:20 +0100

    Customer Test bearbeitet Person Test auf Customer und Employee verteilt Person Test entfällt

--- src/test/java/de/rat/customer/CustomerTest.java
+++ src/test/java/de/rat/customer/CustomerTest.java
@@ -1,8 +1,10 @@
 package de.rat.customer;
 
 import de.rat.common.Address;
+import de.rat.common.Role;
 import de.rat.employee.Employee;
 import de.rat.logistics.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,22 +13,18 @@ import java.util.GregorianCalendar;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerTest {
-//Variable declaration
-private Employee empDanny;
-private Employee empMichael;
-private Employee empJonas;
 
-private Customer custMaria;
-private Customer custLudwig;
+    //Variable declaration
+    private Customer custMaria;
+    private Customer custLudwig;
 
-private Address musterhausen;
-private Station station1;
-private Manufacturer bosch;
+    private Address musterhausen;
+    private Station station1;
+    private Manufacturer bosch;
 
-private Tool drill;
-private Tool hammer;
-private Tool welder;
-
+    private Tool drill;
+    private Tool hammer;
+    private Tool welder;
 
     @BeforeEach
     void setUp() {
@@ -34,19 +32,37 @@ private Tool welder;
         bosch = new Manufacturer("Bosch", musterhausen, "Mr Smith", "123456");
         station1 = new Station("S1", 3, musterhausen);
 
+        // Tools
         drill = new Tool("123", bosch, "Bohrer", Category.HANDTOOL, "1-4-5", ToolStatus.AVAILABLE, 3.0);
         hammer = new Tool("12553", bosch, "Hammer", Category.HANDTOOL, "1-4-6", ToolStatus.ISINREPAIR, 2.5);
         welder = new Tool("ewv133", bosch, "Schweißgerät", Category.HANDTOOL, "1-4-7", ToolStatus.ISRENTED, 3.5);
 
-        station1.addToolToBox(welder);
-
+        //Customer
         custMaria = new Customer("Schmidt", "Maria", new GregorianCalendar(2005, GregorianCalendar.AUGUST, 29), "maria.schmidt@web.de",
         "Weimarerlandstraße", 53, 99986, "Dresden", "Germany", "561616310651");
         custLudwig = new Customer("Ebert", "Ludwig", new GregorianCalendar(1937, GregorianCalendar.DECEMBER, 17), "crazyemail@web.de",
         "Bahnhofsstraße", 16, 99067, "Gotha", "Germany", "01236/465854");
+
+        station1.addToolToBox(welder);
     }
 
+    // Customer Information
+    @Test
+    void create_correct_password_for_customer_and_employee(){
+        Assertions.assertEquals("ma290805sc", custMaria.getAccount().getPassword());
+    }
 
+    @Test
+    void set_correct_email_for_the_customer(){
+        Assertions.assertEquals("maria.schmidt@web.de", custMaria.getAccount().getEmail());
+    }
+
+    @Test
+    void set_correct_role_for_the_customer(){
+        Assertions.assertEquals(Role.CUSTOMER, custMaria.getAccount().getRole());
+    }
+
+    // Customer Interactions
     @Test
     void should_put_and_get_tool_in_inventory() {
         custLudwig.putToolInInventory(drill);
@@ -66,10 +82,8 @@ private Tool welder;
         assertNull(custLudwig.getToolFromInventory(hammer));
     }
 
-
     @Test
     void should_get_a_tool_from_the_station_in_the_inventory() {
-
         Boolean checkGetComplete = custLudwig.getToolFromStation(welder,  station1);
         assertEquals(true, checkGetComplete);
     }
@@ -80,4 +94,5 @@ private Tool welder;
         Boolean checkPutComplete = custLudwig.returnToolToStation(drill, station1);
         assertEquals(true, checkPutComplete);
     }
+
 }
--- src/test/java/de/rat/employee/EmployeeTest.java
+++ src/test/java/de/rat/employee/EmployeeTest.java
@@ -1,5 +1,6 @@
 package de.rat.employee;
 
+import de.rat.common.Role;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,27 +19,39 @@ class EmployeeTest {
     void setUp() {
 
 
-        empDanny = new Employee("Steinbrecher", "Danny", new GregorianCalendar(2019, GregorianCalendar.DECEMBER, 15),
-                "Johannesstraße", 5, 99084, "Erfurt", "Germany");
+        empJonas = new Employee("Hecht", "Jonas", new GregorianCalendar(2019, GregorianCalendar.DECEMBER, 15),
+                "Johannesstraße", 5, 99084, "Weimar", "Germany", null);
         empMichael = new Employee("Müller", "Michael", new GregorianCalendar(2017, GregorianCalendar.FEBRUARY, 5),
-                "Michaelistraße", 17, 99086, "Erfurt", "Germany", empDanny);
-        empJonas = new Employee("Casio", "Jonas", new GregorianCalendar(2000, GregorianCalendar.OCTOBER, 30),
-                "Leipzigerstraße", 99, 99084, "Weimar", "Germany", empDanny);
+                "Michaelistraße", 17, 99086, "Erfurt", "Germany", empJonas);
+
     }
 
 
     // Main Test --------------------------------
     @Test
     void should_return_null_if_the_employee_has_no_supervisor(){
-        Assertions.assertNull(empDanny.getSupervisor());
+        Assertions.assertNull(empJonas.getSupervisor());
     }
 
     @Test
     void should_return_the_correct_supervisor_from_this_employee(){
-        Assertions.assertEquals(empDanny, empMichael.getSupervisor());
+        Assertions.assertEquals(empJonas, empMichael.getSupervisor());
     }
 
+    @Test
+    void create_correct_password_for_customer_and_employee(){
+        Assertions.assertEquals("jo151219he", empJonas.getAccount().getPassword());
+    }
 
+    @Test
+    void create_correct_email_for_employee(){
+        Assertions.assertEquals("jonas.hecht@rat.de", empJonas.getAccount().getEmail());
+    }
+
+    @Test
+    void set_correct_role_for_the_employee(){
+        Assertions.assertEquals(Role.EMPLOYEE, empJonas.getAccount().getRole());
+    }
 
 
 }
