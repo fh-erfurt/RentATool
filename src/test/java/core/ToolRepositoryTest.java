a0ce9a5e7d8767e9223dff9c410e8f3d08e022f4
chris <christian.koenig@fh-erfurt.de>
2020-06-15 20:19:13 +0200

    @DataJpaTest

--- src/test/java/core/ToolRepositoryTest.java
+++ src/test/java/core/ToolRepositoryTest.java
@@ -9,10 +9,12 @@ import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestComponent;
 
 @SpringBootTest(classes= AccessingDataJpaApplication.class)
+@DataJpaTest
 public class ToolRepositoryTest {
     ToolRepository rep;
     private static final Logger log= LoggerFactory.getLogger(ToolRepositoryTest.class);
