6faef9ddf5e1d51a942ffae386c6a0c8d5d12fea
chris <christian.koenig@fh-erfurt.de>
2020-06-15 16:50:31 +0200

    spring implemmentation

---
+++ src/main/java/de/rat/storage/core/AccessingDataJpaApplication.java
@@ -0,0 +1,13 @@
+package de.rat.storage.core;
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataJpaApplication {
+
+
+}
