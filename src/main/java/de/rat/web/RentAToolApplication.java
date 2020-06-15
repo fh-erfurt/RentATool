f2172952f831d561de75a46b1e81fded4d785e89
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2020-06-15 17:02:30 +0200

    added spring web basic objects

---
+++ src/main/java/de/rat/web/RentAToolApplication.java
@@ -0,0 +1,13 @@
+package de.rat.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentAToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentAToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/rat/web/RentAToolApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rat.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentAToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
