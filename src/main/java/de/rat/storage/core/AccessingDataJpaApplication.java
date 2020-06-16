d122204545a9ece9ef3dc46d123fed95c969eb81
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2020-06-16 09:31:08 +0200

    Merged with Spring Start Branch

--- src/main/java/de/rat/storage/core/AccessingDataJpaApplication.java
+++ src/main/java/de/rat/storage/core/AccessingDataJpaApplication.java
@@ -2,6 +2,8 @@ package de.rat.storage.core;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO: duplicate from RentAToolApplication
+
 @SpringBootApplication
 public class AccessingDataJpaApplication {
 
--- src/main/java/de/rat/web/RentAToolApplication.java
+++ src/main/java/de/rat/web/RentAToolApplication.java
@@ -3,6 +3,8 @@ package de.rat.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO: duplicate from AccessingDataJpaApplication
+
 @SpringBootApplication
 public class RentAToolApplication {
 
