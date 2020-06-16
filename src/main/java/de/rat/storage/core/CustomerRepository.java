aefdc5b8d3896107f56e3f57682af22530e93e92
chris <christian.koenig@fh-erfurt.de>
2020-06-16 13:18:04 +0200

    actually works

--- src/main/java/de/rat/storage/core/CustomerRepository.java
+++ src/main/java/de/rat/storage/core/CustomerRepository.java
@@ -2,12 +2,11 @@ package de.rat.storage.core;
 
 import de.rat.model.customer.Customer;
 import org.springframework.data.repository.CrudRepository;
-import de.rat.model.logistics.Tool;
 
-import java.util.List;
 
-public interface CustomerRepository extends CrudRepository<Tool,Long>
+
+
+public interface CustomerRepository extends CrudRepository<Customer,Long>
 {
-    List<Customer> findByDescription(String lastName);
-    Tool findById(long id);
+
 }
--- src/test/java/de/rat/web/RentAToolApplicationTests.java
+++ src/test/java/de/rat/web/RentAToolApplicationTests.java
@@ -6,17 +6,18 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.GregorianCalendar;
 
-@SpringBootTest
+@DataJpaTest
 class RentAToolApplicationTests {
 CustomerRepository rep;
 	private static final Logger log = LoggerFactory.getLogger(RentAToolApplicationTests.class);
 
 
-	@Autowired
+
 	CustomerRepository repository;
 	@Test
 	void contextLoads() {
