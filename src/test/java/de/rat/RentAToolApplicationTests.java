e5df1f279ef77162128058c66c5d3d5b95f53ace
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2020-06-17 10:44:59 +0200

    added Dummy class for testing simple database operations

--- src/test/java/de/rat/RentAToolApplicationTests.java
+++ src/test/java/de/rat/RentAToolApplicationTests.java
@@ -1,7 +1,9 @@
-package de.rat.web;
+package de.rat;
 
+import de.rat.model.Dummy;
 import de.rat.model.customer.Customer;
 import de.rat.storage.core.CustomerRepository;
+import de.rat.storage.core.DummyRepository;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,22 +11,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.GregorianCalendar;
+//@DataJpaTest
 
-@DataJpaTest
+@SpringBootTest
 class RentAToolApplicationTests {
 
 	private static final Logger log = LoggerFactory.getLogger(RentAToolApplicationTests.class);
 
 
-	CustomerRepository repository;
+	DummyRepository repository;
 
 	@Test
-	@Autowired
 	void contextLoads() {
 
 
-		repository.save(new Customer("Ebert", "Ludwig"));
+		repository.save(new Dummy("Hans"));
 	}
 
 }
