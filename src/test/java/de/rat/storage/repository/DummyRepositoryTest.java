6d2ce0221322f2895a620a58f3f166b1baafad45
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2020-06-19 15:33:59 +0200

    tested some database connections

--- src/test/java/de/rat/storage/repository/DummyRepositoryTest.java
+++ src/test/java/de/rat/storage/repository/DummyRepositoryTest.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @DataJpaTest
 class DummyRepositoryTest {
@@ -17,7 +19,9 @@ class DummyRepositoryTest {
     private static final Logger log = LoggerFactory.getLogger(DummyRepositoryTest.class);
 
     @Test
+//    @Rollback(false)
     void  Test() {
+
 
         repository.save(new Dummy("Hans", LocalDate.of(2018,5,20)));
 
