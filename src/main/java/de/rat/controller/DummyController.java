5f01228b34094d520265280aa9a656a4307a4102
chris <christian.koenig@fh-erfurt.de>
2020-07-03 20:27:00 +0200

    works, with h2

--- src/main/java/de/rat/controller/DummyController.java
+++ src/main/java/de/rat/controller/DummyController.java
@@ -7,73 +7,69 @@ import de.rat.storage.repository.DummyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 
-@RestController
+@Controller
 public class DummyController {
     @Autowired
     DummyRepository repository;
-
-    @GetMapping("/dummies")
-    List<Dummy> all() {
-        return (List<Dummy>) repository.findAll();
-    }
-
-    @PostMapping("/dummies")
-    Dummy newEmployee(@RequestBody Dummy newDummy) {
-        return repository.save(newDummy);
-    }
-
-    // Single item
-
-    @GetMapping("/dummies/{id}")
-    Dummy one(@PathVariable Long id) {
-
-        return repository.findById(id)
-                .orElseThrow(() -> new DummyNotFoundException(id));
-    }
-
-    @PutMapping("/dummies/{id}")
-    Dummy replaceEmployee(@RequestBody Dummy newDummy, @PathVariable Long id) {
-
-        return repository.findById(id)
-                .map(employee -> {
-                    employee.setName(newDummy.getName());
-                    return repository.save(newDummy);
-                })
-                .orElseGet(() -> {
-                    newDummy.setId(id);
-                    return repository.save(newDummy);
-                });
-    }
-    @DeleteMapping("/dummies/{id}")
-    void deleteDummy(@PathVariable Long id) {
-        repository.deleteById(id);
-    }
-
-
-
-
-
-
-//    @GetMapping("/dummy")
-//    public String welcomeDummy() {
-//        return "addDummy";
+//
+//    @GetMapping("/dummies")
+//    List<Dummy> all() {
+//        return (List<Dummy>) repository.findAll();
 //    }
 //
-//    @GetMapping("/addDummy")
-//    public String addDummy(Dummy aDummy) {
-//        repository.save(aDummy);
-//        return "addDummy";
+//    @PostMapping("/dummies")
+//    Dummy newEmployee(@RequestBody Dummy newDummy) {
+//        return repository.save(newDummy);
 //    }
 //
-//    @RequestMapping(path="/getDummy")
-//    public ModelAndView getDummy(@RequestParam int id) {
-//        ModelAndView mv = new ModelAndView("showDummy");
-//        Dummy aDummy = repository.findById(id);
-//        mv.addObject(aDummy);
-//        return mv;
+//    // Single item
+//
+//    @GetMapping("/dummies/{id}")
+//    Dummy one(@PathVariable Long id) {
+//
+//        return repository.findById(id)
+//                .orElseThrow(() -> new DummyNotFoundException(id));
 //    }
+//
+//    @PutMapping("/dummies/{id}")
+//    Dummy replaceEmployee(@RequestBody Dummy newDummy, @PathVariable Long id) {
+//
+//        return repository.findById(id)
+//                .map(employee -> {
+//                    employee.setName(newDummy.getName());
+//                    return repository.save(newDummy);
+//                })
+//                .orElseGet(() -> {
+//                    newDummy.setId(id);
+//                    return repository.save(newDummy);
+//                });
+//    }
+//    @DeleteMapping("/dummies/{id}")
+//    void deleteDummy(@PathVariable Long id) {
+//        repository.deleteById(id);
+//    }
+
+    @GetMapping("/dummy")
+    public String welcomeDummy() {
+        return "addDummy";
+    }
+
+    @GetMapping("/addDummy")
+    public String addDummy(Dummy aDummy) {
+        repository.save(aDummy);
+        return "addDummy";
+    }
+
+    @RequestMapping(path="/getDummy")
+    public ModelAndView getDummy(@RequestParam int id) {
+        ModelAndView mv = new ModelAndView("showDummy");
+        Dummy aDummy = repository.findById(id);
+        mv.addObject(aDummy);
+        return mv;
+    }
 }
