6015fac17eed69e8632939be8067096cd8316175
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2020-07-03 22:37:57 +0200

    deleted JavaDoc

--- share/JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"de.rat"},{"l":"de.rat.model.billing"},{"l":"de.rat.model.common"},{"l":"de.rat.model.customer"},{"l":"de.rat.model.employee"},{"l":"de.rat.model.logistics"}]
