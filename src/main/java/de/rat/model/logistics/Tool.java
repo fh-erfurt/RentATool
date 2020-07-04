014309d304af71fe3402b40b41509f714c7d2492
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2020-07-04 13:05:17 +0200

    test with @Column(name = "GRAND_TOTAL_AMOUNT", precision=8, scale=2)

--- src/main/java/de/rat/model/logistics/Tool.java
+++ src/main/java/de/rat/model/logistics/Tool.java
@@ -34,7 +34,7 @@ public class Tool extends BaseModel {
     private String stock;
     private ToolStatus toolStatus;  //TODO: how does it look like in the database
 
-    @Digits(integer=5, fraction=2)
+    @Column(name = "RENTPRICE", precision=8, scale=2)
     private double rentPrice;
 
     protected Tool(){};
