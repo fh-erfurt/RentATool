989925f4c18474d3934353ae839e0bedd7142efc
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2020-07-06 20:25:26 +0200

    generate config package

--- src/main/java/de/rat/config/MvcConfig.java
+++ src/main/java/de/rat/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package de.rat.controller;
+package de.rat.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
--- src/main/java/de/rat/config/WebSecurityConfig.java
+++ src/main/java/de/rat/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package de.rat.controller;
+package de.rat.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -29,34 +29,6 @@ import java.io.IOException;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler(
-//                "/webjars/**",
-//                "/img/**",
-//                "/css/**",
-//                "/js/**")
-//                .addResourceLocations(
-//                        "classpath:/META-INF/resources/webjars/",
-//                        "classpath:/static/img/",
-//                        "classpath:/static/css/",
-//                        "classpath:/static/js/");
-//    }
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/*", "/js/**", "/css/**", "/images/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-////                .loginPage("")
-//                .permitAll()
-//                .and()
-//                .logout();
-//
-//    }
-
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -85,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .antMatchers("/ADMIN").hasRole("ADMIN")
                     .antMatchers("/EMPLOYEE").hasAnyRole("EMPLOYEE", "ADMIN")
-                    .antMatchers("/CUSTOMER").hasAnyRole("CUSTOMER","EMPLOYEE", "ADMIN")
+                    .antMatchers("/CUSTOMER", "/tools/*").hasAnyRole("CUSTOMER","EMPLOYEE", "ADMIN")
                     .and()
                 .formLogin()
                     .loginPage("/login")
@@ -100,46 +72,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling()
                     .accessDeniedHandler(new CustomAccessDeniedHandler()).and()
                     .exceptionHandling().authenticationEntryPoint(new CustomHttp403ForbiddenEntryPoint())
-                    .accessDeniedPage("/ERROR");;
-
-
-
-
-//
-//
-//        // level administrator
-//        http.authorizeRequests()
-//                .antMatchers("/ADMIN").hasRole("ADMIN")
-//                .and().formLogin().permitAll();
-//
-//        // level employee
-//        http.authorizeRequests()
-//                .antMatchers("/EMPLOYEE").hasAnyRole("EMPLOYEE", "ADMIN")
-//                .and().formLogin().permitAll();
-//
-//        // level customer
-//        http.authorizeRequests()
-//                .antMatchers("/CUSTOMER").hasAnyRole("CUSTOMER","EMPLOYEE", "ADMIN")
-//                .and().formLogin().permitAll();
-//
-//        // level unknown user
-//        http.authorizeRequests()
-//                .antMatchers("/", "/home", "/START", "/logout", "/ERROR").permitAll();
-//
-//        // deny accsecc to all other sites
-//        http.authorizeRequests().antMatchers("/*").denyAll();
-//
-//
-//        http.exceptionHandling()
-//                .accessDeniedHandler(new CustomAccessDeniedHandler()).and()
-//                .exceptionHandling().authenticationEntryPoint(new CustomHttp403ForbiddenEntryPoint())
-//                .accessDeniedPage("/ERROR");;
-//
-//        // logout
-//        http.logout().logoutUrl("/logout")
-//                .logoutSuccessUrl("/START");
-
+                    .accessDeniedPage("/ERROR");
     }
+
 
     public static class CustomHttp403ForbiddenEntryPoint implements AuthenticationEntryPoint {
 
@@ -148,14 +83,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                              AuthenticationException authException) throws IOException {
             response.sendError(HttpServletResponse.SC_FORBIDDEN,"Access denied");
         }
-
     }
 
     public static class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
         @Override
-        public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException arg2) {}
-
+        public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException arg2)
+                throws IOException, ServletException {
+            response.getWriter().print("You don't have required role to perform this action.");
+        }
     }
 
 
