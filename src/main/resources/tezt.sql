a9b24640300c2b279e400aacf0725ae39df3d238
chris <christian.koenig@fh-erfurt.de>
2020-07-07 19:46:51 +0200

    sql

--- src/main/resources/tezt.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO Login_User(id, user_name, password, active, roles) VALUES (1,'user1', 'pass', false, 'CUSTOMER');
-INSERT INTO Login_User(id, user_name, password, active, roles) VALUES (2,'user2', 'pass', true, 'CUSTOMER');
-INSERT INTO Login_User(id, user_name, password, active, roles) VALUES (3,'user3', 'pass', true, 'ADMIN');
