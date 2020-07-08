c7afabbf78f2bbb4a5ea8f261c382e2d919badb9
monschey <marco.petzold@fh-erfurt.de>
2020-07-08 09:49:27 +0200

    added WarehouseRepositoryTest

--- src/main/java/de/rat/storage/repository/AddressRepository.java
+++ src/main/java/de/rat/storage/repository/AddressRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-
 public interface AddressRepository extends CrudRepository<Address,Integer> {
 
     Address findById(int id);
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ INSERT INTO Login_User(id, user_name, password, active, roles) VALUES (3,'user3'
 
 
 INSERT into ADDRESS (id, city,country,house_nr,street,zip) values (1,'Weimar','de',87,'Poststraße',05687);
-INSERT into ADDRESS (id, city,country,house_nr,street,zip) values (2,'Arnstadt','de',87,'Musterstraße',05687);
+INSERT into ADDRESS (id, city,country,house_nr,street,zip) values (2,'Erfurt','de',87,'Musterstraße',05687);
 INSERT into ADDRESS (id, city,country,house_nr,street,zip) values (3,'Gotha','de',87,'Asbachstraße',05687);
 
 
