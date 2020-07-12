d809bd8f00df7c1243f6eb53a9d65d80a8d38c96
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2020-07-13 00:20:51 +0200

    added userManagement added customer.html added employee.html

--- src/main/java/de/rat/config/MvcConfig.java
+++ src/main/java/de/rat/config/MvcConfig.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration // TODO: delete this Config, because we route this in our controller
-public class MvcConfig implements WebMvcConfigurer {
-
-    public void addViewControllers(ViewControllerRegistry registry) {
-
-        registry.addViewController("/ADMIN").setViewName("ADMIN");
-        registry.addViewController("/EMPLOYEE").setViewName("EMPLOYEE");
-        registry.addViewController("/CUSTOMER").setViewName("CUSTOMER");
-        registry.addViewController("/START").setViewName("START");
-
-    }
-}
+//@Configuration // TODO: delete this Config, because we route this in our controller
+//public class MvcConfig implements WebMvcConfigurer {
+//
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//
+//        registry.addViewController("/ADMIN").setViewName("ADMIN");
+//        registry.addViewController("/EMPLOYEE").setViewName("EMPLOYEE");
+//        registry.addViewController("/CUSTOMER").setViewName("CUSTOMER");
+//        registry.addViewController("/START").setViewName("START");
+//
+//    }
+//}
