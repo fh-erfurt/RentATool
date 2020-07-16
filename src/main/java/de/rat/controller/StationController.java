8cbf57e2cb1f71065658f3a26311ebf1bb763641
monschey <marco.petzold@fh-erfurt.de>
2020-07-16 16:40:19 +0200

    rent tool and choose station added

--- src/main/java/de/rat/controller/StationController.java
+++ src/main/java/de/rat/controller/StationController.java
@@ -1,27 +1,55 @@
 package de.rat.controller;
 
-import de.rat.model.logistics.Station;
-import de.rat.model.logistics.Tool;
+import de.rat.model.logistics.*;
 import de.rat.storage.repository.StationRepository;
+import de.rat.storage.repository.ToolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Iterator;
 import java.util.List;
 
 @Controller
 public class StationController {
     @Autowired
     StationRepository stationRepository;
+    @Autowired
+    ToolRepository toolRepository;
+    private static final Logger log = LoggerFactory.getLogger(ToolController.class);
 
-    @RequestMapping(path="/setStation")
-    public String listAllTools(Model model)
+    @RequestMapping(path="/chooseStation/{id}")
+    public String listAllTools(@PathVariable(name = "id") int id,Model model)
     {
+        log.info("22222");
+        Tool rentedTool = toolRepository.findById(id);
         List<Station> listStation= (List<Station>) stationRepository.findAll();
 
+        for (Station station : listStation) {
+            log.info("1111");
+            log.info(String.valueOf(station.getId()));
+        }
         model.addAttribute("listStation", listStation);
+        model.addAttribute(rentedTool);
 
         return "chooseStation";
+    }
+
+
+    @PostMapping("/setStation/{toolId}/{stationId}")
+    public String rentATool(@PathVariable("toolId") int toolId,@PathVariable("stationId") int stationId)
+    {
+        Station rentStation = stationRepository.findById(stationId);
+        Tool rentedTool = toolRepository.findById(toolId);
+        log.info(rentedTool.getDescription());
+        log.info(rentStation.getDescription());
+
+        return "rentSuccessful";
     }
 }
